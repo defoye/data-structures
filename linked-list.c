8f10330d05321d7ffc37118fbb4950b3f312cfc0
Ernest D DeFoy III <erniedefoy3@gmail.com>
2019-09-26 15:17:28 -0400

    Add deleteList, test deleteList

--- linked-list.c
+++ linked-list.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 struct Node {
-  int *data;
-  struct Node *next;  
+    int *data;
+    struct Node *next;
+	struct Node *prev;
 };
 
 struct LinkedList {
-  struct Node *head;
-  struct Node *tail; 
+  	struct Node *head;
+  	struct Node *tail;
 };
 
 struct Node* createNode() {
 	struct Node *node = (struct Node*)malloc(sizeof(struct Node*));
 	node->data = (int*)malloc(sizeof(int));
 	node->next = NULL;
+	node->prev = NULL;
 	
 	return node;
 }
@@ -23,6 +28,34 @@ struct Node* createNode() {
 void freeNode(struct Node *node) {
 	free(node->data);
 	free(node);
+}
+
+void deleteNode(struct Node *node) {
+	if(node == NULL) { return; }
+//	struct Node *prev = node->prev;
+//	struct Node *next = node->next;
+	
+	if(node->prev != NULL) {
+		node->prev->next = node->next;
+	}
+	if(node->next != NULL) {
+		node->next->prev = node->prev;
+	}
+	
+	freeNode(node);
+}
+
+void deleteList(struct Node *node) {
+	if(node == NULL) { return; }
+	
+	struct Node *next;
+	
+	while(node != NULL) {
+		next = node->next;
+		deleteNode(node);
+		printf("deleted");
+		node = next;
+	}
 }
 
 void printList(struct Node *head) {
@@ -51,6 +84,7 @@ struct Node* dummyData() {
 		if(i == 0) {
 			head = curr;
 		}
+		curr->prev = prev;
   		prev = curr;
   	}
 	
@@ -67,11 +101,15 @@ void testPtr() {
 	assert(*(head->data) == *(node->data));
 }
 
+void testInitDeinit() {
+	struct Node *head = dummyData();
+	
+	deleteList(head);
+}
+
 int main(void) {
 
-	testPtr();
-	struct Node *head = dummyData();
-	printList(head);
-	
+//	testPtr();
+	testInitDeinit();
   	return 0;
 }
